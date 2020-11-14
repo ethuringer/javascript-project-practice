3a7b98c5fc3a7b81deceaf8fb0bb5bc2e76310ad
ethuringer <ethuringer@gmail.com>
2020-11-14 17:53:57 +0100

    delete algo

--- js/function.js
+++ js/function.js
@@ -1,4 +1,7 @@
+'use strict';
 
+
+// 1. feladat
 console.log(document.querySelectorAll(".button"));
 
 /* function handleClick() {
@@ -7,3 +10,16 @@ for (i = 0; i < buttons.length; i += 1);
 
 }
  */
+
+ // 2. feladat
+
+const summation = (a = 0, b = 0) => a + b;
+console.log(summation(20));
+
+const subtraction = (a = 0, b = 0) => a - b;
+console.log(subtraction(45));
+
+// 3. feladat
+
+const personDatalog = (firstName = 'John', lastName = 'Doe', age = '33') => console.log(`My name is ` + firstName +` ` + lastName + `. I'm ` + age + ` years old.`);
+personDatalog();
