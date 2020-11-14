5245824f5d4ee8b2cd8dbcc75d52bacb78a91966
ethuringer <ethuringer@gmail.com>
2020-11-14 17:08:41 +0100

    create algo file

---
+++ js/function.js
@@ -0,0 +1,9 @@
+
+console.log(document.querySelectorAll(".button"));
+
+/* function handleClick() {
+    const buttons = document.getElementsByClassName("button");
+for (i = 0; i < buttons.length; i += 1);
+
+}
+ */
